91ec03d15b4a88e429fd1e8e8739a43be681e387
Chinmya <105976402+chinmayabeheragit@users.noreply.github.com>
2024-12-03 21:08:32 +0530

    started the podcast ai project

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import LeftSideBar from "@/components/LeftSideBar";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="relative flex flex-col">
+        <main className="relative flex  bg-black-3">
+            <LeftSideBar />
+            {children}
+            <p className="text-white-1">RIGHT SIDEBAR</p>
+        </main>
+    </div>
+
+  );
+}

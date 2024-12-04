4e1234ca8827f231a1806857cd9bdde16fb882c2
Chinmya <105976402+chinmayabeheragit@users.noreply.github.com>
2024-12-05 01:44:49 +0530

    push after complete convex setup clerk setup home page layout desgine

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+//https://equal-terrier-46.clerk.accounts.dev
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://your-issuer-url.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
